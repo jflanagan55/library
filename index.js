f41549981bc6a23ccf7e94a5f130dc1b3b659401
Justin Flanagan <jflanagan5555@gmail.com>
2022-03-27 18:02:31 -0400

    in need of some styling but solid otherwise

--- index.js
+++ index.js
@@ -18,13 +18,13 @@ class Book{
       myLibrary.push(book)
     }
   }
-
+  
   }
 
 
+  class displayFunctions{
 
-
-    function addBookToDisplay(book){
+  static  addBookToDisplay(book){
       let newRow = document.createElement('tr');
       newRow.classList.add('tableRows')
       let {title, author, pages, haveRead} = book;
@@ -41,16 +41,46 @@ class Book{
       pagesCell.innerText = pages;
       newRow.append(pagesCell)
 
-      let haveReadCell= document.createElement('td');
-      haveReadCell.innerText = haveRead;
+      let haveReadCell = document.createElement('td')
+      let haveReadButton= document.createElement('button');
+      haveReadButton.classList.add('readStatusButton')
+      if(haveRead === true){
+      haveReadButton.innerText = "Yes";
+      }
+      else{
+        haveReadButton.innerText = "No"
+      }
       newRow.append(haveReadCell)
+      haveReadCell.append(haveReadButton)
+
+      let deleteCell = document.createElement('td')
+      let deleteBtn = document.createElement("button");
+      deleteBtn.classList.add('delete');
+      deleteBtn.innerText = "Delete"
+      newRow.append(deleteBtn)
 
       let libData = document.querySelector('.libData');
       libData.append(newRow)
 
+      deleteBtn.addEventListener('click', () =>{
+        let rowRemove = deleteBtn.parentElement;
+        let dad = rowRemove.parentElement;
+        dad.removeChild(rowRemove);
+        capture: true
+        
+            
+      })
+      haveReadButton.addEventListener('click', ()=>{
+        if(haveReadButton.innerText === "Yes"){
+          haveReadButton.innerText = "No"
+        }
+        else {haveReadButton.innerText = "Yes"}
+      })
+      
+
 
     }
-  
+  }
 
   
 let submitBtn = document.querySelector('#submit')
@@ -60,15 +90,21 @@ submitBtn.addEventListener('click', ()=>{
   let titleInfo = document.querySelector("#title").value
   let authorInfo = document.querySelector("#author").value
   let pagesInfo = document.querySelector("#pages").value
-  let haveReadInfo = document.querySelector('#haveRead').value
+  let haveReadInfo = document.querySelector('#haveRead').checked
   let book = new Book(titleInfo, authorInfo, pagesInfo, haveReadInfo)
   book.addBookToLibrary(book)
-  addBookToDisplay(book)
+  displayFunctions.addBookToDisplay(book)
+  clearForm()
 })
 
 
+function clearForm(){
+  document.querySelector('#title').value = ""
+  document.querySelector('#author').value= ""
+  document.querySelector('#pages').value= ""
+  document.querySelector('#haveRead').checked = false
 
-
+}
 
 
 
